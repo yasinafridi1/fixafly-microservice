50bba09757713720ff64ea96770d49e12ba90884
yasinafridi1 <yaseenafridi10875@gmail.com>
2025-08-31 04:43:06 +0500

    change lat long array

--- services/technician/helpers/location.js
+++ services/technician/helpers/location.js
@@ -1,14 +1,14 @@
 export const locationObjBuilder = (lat, lng) => {
   const location = {
     type: "Point",
-    coordinates: [lat, lng],
+    coordinates: [lng, lat],
   };
 
   return location;
 };
 
 export const locationObjDesctructure = (location) => {
-  const lat = location.coordinates[0];
-  const lng = location.coordinates[1];
+  const lng = location.coordinates[0];
+  const lat = location.coordinates[1];
   return { lat, lng };
 };
